89bf3b5d781cf0c41a32f281214638085b4a95a7
Youngji, Cho <cyj9058@gmail.com>
2016-08-01 23:00:03 +0900

    Update Korea-power-plant-analysis.R

--- Korea-power-plant-analysis.R
+++ Korea-power-plant-analysis.R
@@ -4,3 +4,8 @@ library(KoNLP)
 power.plant<-read.xlsx2("power-plant.xlsx",sheetIndex = 1)
 level<-levels(power.plant$연료원)[2:length(levels(power.plant$연료원))]
 first<-grep(".",power.plant$연료원)
+
+for(i in first)
+{
+  name<-paste0(power.plant$연료원[i],"/",power.plant$발전형식[i])
+}
