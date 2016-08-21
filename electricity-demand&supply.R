25ac1c66685ecbb01e0545b4e2057c57840ea22d
Youngji, Cho <cyj9058@gmail.com>
2016-08-21 15:01:25 +0900

    Update electricity-demand&supply.R

--- electricity-demand&supply.R
+++ electricity-demand&supply.R
@@ -1,6 +1,7 @@
 library(xlsx)
 library(lubridate)
 library(ggplot2)
+
 elec<-read.xlsx2(file="electricity_supply.xlsx",sheetIndex = 1)
 names(elec)<-c("기간","설비용량","공급능력","최대전력","공급예비력","공급예비율","기준일시")
 elec<-elec[1:100, ]
@@ -10,6 +11,9 @@ elec$공급예비력<-as.integer(elec$공급예비력)
 elec$공급예비율<-as.integer(elec$공급예비율)
 elec$공급예비력<-elec$공급예비력*10
 
-ggplot(elec,aes(기간,공급예비력))
-+geom_point(aes(x=기간, 공급예비력))
-+theme_bw(12,base_family = "AppleGothic")
+ggplot(elec,aes(x=기간,y=공급예비력))+
+  geom_bar(stat = "identity")+
+  theme_bw(base_family = "AppleGothic")+
+  ylab("공급예비력(Mw)")+
+  xlab("기간(최근 100일)")+
+  ggtitle("최근 100일 전력수급현황(16년 8월 20기준)")
