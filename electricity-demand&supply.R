e44bf36428bd6db06b7f73f975ee6b58d299233b
Youngji, Cho <cyj9058@gmail.com>
2016-08-22 20:42:04 +0900

    Update electricity-demand&supply.R

--- electricity-demand&supply.R
+++ electricity-demand&supply.R
@@ -28,19 +28,19 @@ term<-paste0("(",start.date,"~",end.date,")")
 
 reserve.graph<-ggplot(elec,aes(x=기간,y=공급예비력,colour=공급예비력))+
   geom_point(stat = "identity")+
-  geom_hline(yintercept=5000,colour="red",linetype="dashed",show.legend=TRUE)+
-  scale_linetype_manual(name="비상",values = 5000)+
+  geom_hline(aes(yintercept=5000,linetype="전력수급비상 1단계(5,000Mw)"),colour="red",show.legend=TRUE)+
   theme_bw(base_family = "AppleGothic")+ 
   ylab("공급예비력(Mw)")+
   xlab(paste("기간",term))+
-  ggtitle(paste("공급예비력",term)) #공급예비력 계산 
+  ggtitle(paste("전력 공급예비력(Mw)",term)) #공급예비력 계산 
 
 capacity.graph<-ggplot(elec,aes(x=기간,y=공급예비율,group=1))+
   geom_line(stat = "identity")+
-  geom_hline(yintercept=c(5,15,22),colour="red",linetype="dashed", size=1)+
   theme_bw(base_family = "AppleGothic")+
   ylab("발전예비율(%)")+
   xlab(paste("기간",term))+
-  ggtitle(paste("전력수급현황",term)) #공급예비율 계산 
-
+  ggtitle(paste("전력예비율(%)",term))+
+  geom_hline(aes(yintercept=10,linetype="비상단계(10%)"), colour="red",show.legend=TRUE)+
+  geom_hline(aes(yintercept=15,linetype= "OECD 평균 예비율(15%)"), colour= "blue",show.legend=TRUE)+
+  geom_hline(aes(yintercept=22,linetype="국내 전력수급계획 예비율(22%)"), colour="green",show.legend=TRUE)
   
