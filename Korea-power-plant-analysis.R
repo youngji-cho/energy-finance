d032f265dfcd6acf2c6b07f5eb5d8be72244d175
Youngji, Cho <cyj9058@gmail.com>
2016-09-01 21:08:27 +0900

    Update Korea-power-plant-analysis.R

--- Korea-power-plant-analysis.R
+++ Korea-power-plant-analysis.R
@@ -1,23 +1,31 @@
-
 setwd("~/Dropbox/Programming/R/data")
-library(tm)
+library(xlsx)
+library(dplyr)
 library(KoNLP)
+
 power.plant<-read.xlsx2("power-plant.xlsx",sheetIndex = 1)
 level<-levels(power.plant$연료원)[2:length(levels(power.plant$연료원))]
 first<-grep(".",power.plant$연료원)
 
-j<-1
-name<-vector(mode="character",length = length(first))
-for(i in first)
-{
-  print(j)
-  print(i)
-  name[j]<-paste0(power.plant$연료원[i],".",power.plant$발전형식[i])
-  j<-j+1
-}
+section<-function(keyword){
+  val1<-grep(keyword,power.plant$연료원)
+  val2<-which(first==val1)
+  val3<-val2+1
+  
+  sectioned1<-power.plant[first[val2]:first[val3],]
+  sectioned2<-select(sectioned1,발전소명:총설비용량)
+  sectioned3<-sectioned2[1:(length(sectioned2$발전소명)-3),]
+  sectioned4<-arrange(sectioned3,desc(총설비용량))
+  
+  return(sectioned4)
+  }
 
-for(1 in length(first)){
-  subsetted<-power.plant[first[i]:first[i+1],]
-  assign(name[i],subsetted)
+disslike.shap<-function(x){
+  num<-grep(x$발전소명,pattern="#")
+  x.no<-x[-(num),]
+  x.yes<-x[num,]
+  
+  ordered<-x.yes[order(x.yes$발전소명),]
+  
+  
 }
-
